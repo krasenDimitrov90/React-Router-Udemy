e1ee3539aeb9c840ba1980c8d9bb40f6705bf176
krasenDimitrov <krasendimitrov90@gmail.com>
2024-03-10 20:30:21 +0200

    adding a welcome page

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { PageContent } from '../components/PageContent';
 
 export const HomePage = () => {
   return (
-    <h1>Home</h1>
+    <PageContent title={'Welcome'}>
+      <p>Browse our amazing events!</p>
+    </PageContent>
   );
 };
